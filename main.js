29129e36b31436ca43125ef0d4df7b0e03df4205
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-09-12 17:27:30 -0400

    Fixed modal element

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function remModal() {
   elem.style.visibility = "hidden";
 }
 if (location.protocol != "https:") {
+  var rootLink = "https:" + window.location.href.substring(window.location.protocol.length);
   var elem = document.createElement("span");
   elem.style.userSelect = "none";
   elem.style.top = "0";
@@ -14,7 +15,7 @@ if (location.protocol != "https:") {
   elem.style.width = "200px";
   elem.style.lineHeight = "20px"
   elem.style.textAlign = "center";
-  elem.innerHTML = "<h3>Hello there!</h3>It looks like you're not connected through https. <a href = \"https://searchme.one\" id = \"http\">Secure my connection</a><br /><button style = \"cursor: pointer;\" onclick = \"remModal()\">&times;</button>";
+  elem.innerHTML = "<h3>Hello there!</h3>It looks like you're not connected through https. <a href = \"" + rootLink + "\" id = \"http\">Secure my connection</a><br /><button style = \"cursor: pointer;\" onclick = \"remModal()\">&times;</button>";
   elem.style.color = "white";
   elem.style.fontFamily = "'Mukta', sans-serif";
   document.body.appendChild(elem);
